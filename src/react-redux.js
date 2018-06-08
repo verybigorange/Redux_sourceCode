ddfe15c896558b0de1f6acd3c503731893b37865
verybigorange <449732828@qq.com>
2018-06-08 17:28:11 +0800

    connect在shouldUpdateComponent增加比较

--- src/react-redux.js
+++ src/react-redux.js
@@ -29,29 +29,42 @@ export function connect(mapStateToProps,mapStateToDispatch) {
 
                 // 将组件自己的属性、状态树上需要的属性，以及更新reducer的方法合并传入到该组件中。
                 this.store = props.store || context.store;
+                
                 this.state = {
-                    nextProps: this.merge()
+                    nextProps:this.merge()
                 }
-    
+
                 context.store.subscribe(()=>{
-                    this.update()
+                    this.setState({
+                        nextProps:this.merge()
+                    })
                 })
               
             }
+            // 浅比较
+            hasChanged(old,next){
+                if(old === next) return false
+
+                if(Object.keys(old).length !== Object.keys(next).length) return true
+                
+                // 属性名以及属性值是否相同
+                for(let key in old){
+                    if(!next.hasOwnProperty(key)) return true
+                    if(old[key] !== next[key]) return true
+                }
+                return false
+            }
+
+            shouldComponentUpdate(nextProps,nextState){
+                return this.hasChanged(this.state.nextProps,nextState)
+            }
 
             merge(){
-              
                 return {
                     ...this.props,
                     ...mapStateToProps(this.store.getState(),this.props),
                     ...mapStateToDispatch(this.store.dispatch)
                 }
-            }
-
-            update(){
-                this.setState({
-                    nextProps:this.merge()
-                })
             }
 
             render(){
